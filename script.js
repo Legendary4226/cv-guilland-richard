ec7c7463b40fe7f11f2193d7a49023c75b1b8219
Legendary4226 <42912228+Legendary4226@users.noreply.github.com>
2021-10-10 17:54:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Permet lors du clic sur le bouton d'exécuter cette fonction, elle change la classe de body à l'inverse de son état actuel
+function toggle_dark_light_theme() {
+    document.documentElement.classList.toggle("white-theme");
+}
